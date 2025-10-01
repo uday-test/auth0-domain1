5a853b74ac9a940a23450303512fdaac5e6907a9
Uday Bhaskar Voora <udayvoora@gmail.com>
2025-09-30 18:55:50 -0700

    fix: Update workflows to use AUTH0_* environment variables

--- base/terraform/main.tf
+++ base/terraform/main.tf
@@ -8,9 +8,7 @@ terraform {
 }
 
 provider "auth0" {
-  domain        = var.auth0_domain
-  client_id     = var.auth0_client_id
-  client_secret = var.auth0_client_secret
+  # Reads from AUTH0_DOMAIN, AUTH0_CLIENT_ID, AUTH0_CLIENT_SECRET env vars
 }
 
 # Load config.yml from base folder
